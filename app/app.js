007aeb084964f726960879614a34bb967be15cec
Francois van der Merwe <swyser@live.co.za>
2016-02-15 22:18:50 +0200

    Great progress!
    I have changed allot of things! I have changed all the wording,
    theming, added some new controllers, new views and started with the
    upload of messages and pictures. Yaaargggg!

--- app/app.js
+++ app/app.js
@@ -2,25 +2,39 @@
 
 var rpiApp = angular.module('rpiApp', ['ngRoute', 'ngMaterial']);
 
-rpiApp.config(function($routeProvider){
+rpiApp.config(function($routeProvider) {
   $routeProvider.
   when('/home', {
-      templateUrl: 'app/views/homeView.html',
-      controller: 'HomeController'
+    templateUrl: 'app/views/homeView.html',
+    controller: 'HomeController'
   }).
   when('/photos', {
-      templateUrl: 'app/views/photosView.html',
-      controller: 'PhotosController'
+    templateUrl: 'app/views/photosView.html',
+    controller: 'PhotosController'
   }).
   when('/messages', {
-      templateUrl: 'app/views/messagesView.html',
-      controller: 'MessageController'
+    templateUrl: 'app/views/messagesView.html',
+    controller: 'MessageController'
   }).
   when('/upload', {
-      templateUrl: 'app/views/uploadView.html',
-      controller: 'UploadController'
+    templateUrl: 'app/views/uploadView.html',
+    controller: 'UploadController'
+  }).
+  when('/about', {
+    templateUrl: 'app/views/aboutView.html',
+    controller: 'AboutController'
   }).
   otherwise({
-      redirectTo: '/home'
+    redirectTo: '/home'
   });
+});
+
+
+rpiApp.config(function($mdThemingProvider) {
+    $mdThemingProvider
+    .theme('default')
+    .primaryPalette('brown')
+    .accentPalette('light-blue')
+    .warnPalette('red')
+    .backgroundPalette('blue-grey');
 });
---
+++ app/controllers/aboutController.js
@@ -0,0 +1,7 @@
+(function(){
+  'use strict';
+
+  rpiApp.controller('HomeController', function(){
+
+  });
+})();
